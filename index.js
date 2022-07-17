1ef789f6306051605c0d08d36ef340b5d5045329
S Rabi <rabi.sanaye@yahoo.com>
2022-07-17 18:50:50 +0200

    c

--- index.js
+++ index.js
@@ -2,18 +2,29 @@
 
 // Otherwise returns false:
 
-
 function checkAge(age) {
-
-  }
-
+  return age > 18;
+}
+console.log(checkAge(20));
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
-  function min(a, b) {
-   
+function min(a, b) {
+  if (a > b) {
+    return b;
+  } else {
+    return a;
   }
+}
+console.log(min(2, 5));
+console.log(min(3, -1));
+console.log(min(1, 1));
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
-  function pow(x, n) {
-    
-  }
+
+function pow(a, b) {
+  let result = a ** b;
+  console.log(result);
+}
+pow(3, 2);
+pow(3, 3);
+pow(1, 100);
